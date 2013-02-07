9af65de03b9d34da11ef179b16e965e0209f0d46
Josh Faust <josh.faust@gmail.com>
2013-02-06 16:18:51 -0800

    fix the windows build script for qt5

--- st_build_windows_common.ps1
+++ st_build_windows_common.ps1
@@ -1,7 +1,30 @@
 $osname = $args[0]
 
+# Grab ICU, which is required for building webkit
+echo "downloading ICU..."
+$webclient = New-Object System.Net.WebClient
+$webclient.DownloadFile("http://repo.suitabletech.com/downloads/icu4c-50_1_2-$osname-msvc10.zip", "$(get-location)/icu.zip")
+
+function unzip($filename) 
+{ 
+  if (!(test-path $filename)) { throw "$filename does not exist" } 
+  $shell = new-object -com shell.application 
+  $shell.namespace($pwd.path).copyhere($shell.namespace((join-path $pwd $filename)).items(), 0x14) 
+}
+
+# Unzipping icu
+echo "unzipping ICU..."
+unzip("icu.zip")
+
+$icu_libdir = "$(get-location)\icu\lib"
+if ($osname -eq "win64")
+{
+  $icu_libdir = "$(get-location)\icu\lib64"
+}
+
+$version = $(git rev-parse HEAD)
 echo configuring ...
-.\configure.exe -debug-and-release -opensource -confirm-license -shared -webkit -mp -no-phonon
+.\configure.bat -debug-and-release -force-debug-info -opensource -confirm-license -shared -nomake tools -nomake docs -nomake examples -nomake demos -nomake tests -mp -icu -prefix "$(get-location)\$version" -I "$(get-location)\icu\include" -L "$(get-location)\icu\lib" -L "$icu_libdir"
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
@@ -10,37 +33,16 @@ nmake
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
-echo cleaning ...
-nmake clean
-
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-
-rm lib/*.dll
-rm -R -Force examples
-rm -R -Force demos
-rm -R -Force doc
-rm -R -Force tests
-ls . -R -include *.obj | rm -Force
-
-if ($LastExitCode -ne 0) { exit $LastExitCode }
-
-$version = $(git rev-parse HEAD)
-$tmpdir = "$env:TEMP/$version"
-
-echo $tmpdir
-
-mkdir $tmpdir
-
-echo "copying qt to $tmpdir ..."
-cp -R -Force -Exclude .git * $tmpdir
+echo installing...
+nmake install
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
 echo creating tarball...
-push-location
-cd "$tmpdir/.."
 cmake -E tar cvzf "qt-$version-$osname.tar.gz" "$version"
-pop-location
-mv "$tmpdir/../qt-$version-$osname.tar.gz" .
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }
+
+# Delete the version folder, since the way teamcity cleans things having a folder that's
+# also a revision is bad
+rm -Force -Recurse ./$version

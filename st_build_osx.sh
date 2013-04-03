03748afb461f1b10ad547af53bde6587909f2506
Josh Faust <jfaust@suitabletech.com>
2013-04-03 16:50:21 -0700

    update module versions

--- st_build_osx.sh
+++ st_build_osx.sh
@@ -5,12 +5,7 @@ set -e
 version=`$TEAMCITY_GIT_PATH rev-parse HEAD`
 install_dir=$PWD/$version
 
-# Make sure the 10.6 sdk is installed. Unfortunately as of 5.1 Qt does not support just giving a path.
-#xcodebuild -sdk macosx10.6 -version
-#OSX_SDK=osxsdk-10.6-osx.tar.gz
-#curl -o $OSX_SDK http://repo.suitabletech.com/downloads/osxsdk/$OSX_SDK
-#tar xvzf $OSX_SDK
-./configure -prefix $install_dir -release -opensource -confirm-license -shared -nomake examples -nomake demos -nomake docs -no-c++11 -platform macx-g++
+./configure -prefix $install_dir -release -opensource -confirm-license -shared -nomake examples -nomake demos -nomake docs -no-c++11 -platform macx-clang
 
 make -j8
 make install

83ed8711d88bf65908605004d232114382a1181e
Josh Faust <jfaust@suitabletech.com>
2013-06-23 13:52:25 -0700

    fix configure line for no-longer-existing nomake targets

--- st_build_linux.sh
+++ st_build_linux.sh
@@ -7,7 +7,7 @@ arch=$2
 version=`git rev-parse HEAD`
 install_dir=$PWD/$version
 
-./configure -prefix $install_dir -release -platform linux-g++-$bits -opensource -confirm-license -shared -nomake examples -nomake demos -nomake docs -nomake tests -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape -opengl -fontconfig -qt-xcb -gtkstyle -qt-libjpeg
+./configure -prefix $install_dir -release -platform linux-g++-$bits -opensource -confirm-license -shared -nomake examples -nomake tests -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape -opengl -fontconfig -qt-xcb -gtkstyle -qt-libjpeg
 
 make -j8
 make install

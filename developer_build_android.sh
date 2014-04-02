85fba7b0b116a7d67f49ca066ad0bb323c430e3f
Daiwei Li <daiweili@suitabletech.com>
2014-04-01 21:57:21 -0700

    Update Android build scripts to take an arch

--- developer_build_android.sh
+++ developer_build_android.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./configure -developer-build -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors
+arch=${1:-armeabi-v7a}
+
+./configure -developer-build -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors
 
 make -j8
--- st_build_android.sh
+++ st_build_android.sh
@@ -3,15 +3,16 @@
 set -e
 version=`$TEAMCITY_GIT_PATH rev-parse HEAD`
 install_dir=$PWD/$version-android
+arch=${1:-armeabi-v7a}
 
-./configure -prefix $install_dir -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors
+./configure -prefix $install_dir -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors
 
 make -j8
 # TODO generate and upload symbols
 
 make install
 
-tar cvzf qt-$version-android.tar.gz $install-dir
+tar cvzf qt-$version-android-$arch.tar.gz $install-dir
 # Delete the version folder, since the way teamcity cleans things having a folder that's
 # also a revision is bad
 rm -rf $install-dir

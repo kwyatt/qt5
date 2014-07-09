b9da80241fe61c31128f188dccdb6f07c4a26594
Daiwei Li <daiweili@suitabletech.com>
2014-07-09 15:36:00 -0700

    Don't build with bearer management

--- developer_build_android.sh
+++ developer_build_android.sh
@@ -2,6 +2,6 @@
 
 arch=${1:-armeabi-v7a}
 
-./configure -developer-build -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors
+./configure -developer-build -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors -no-feature-bearermanagement -no-compile-examples
 
 make -j8

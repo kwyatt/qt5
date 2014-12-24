c6a75328407ffc11cc0a6fdde6f27be8b5031876
Daiwei Li <daiweili@suitabletech.com>
2014-12-24 02:36:08 -0800

    Upgrade submodules to 5.4.0
    Still need to flesh out scripts to make future upgrades easy
    OSX now builds with C++11 and harfbuzz

--- push_st_to_github.py
+++ push_st_to_github.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import subprocess
 import sys
+import argparse
 
 modules = [
   'qtbase',
@@ -11,11 +12,25 @@ modules = [
   'qtwebkit',
 ]
 
+def has_branch(branch, modulepath):
+  return subprocess.call(['git', 'show-ref', '--verify', '--quiet', 'refs/heads/%s' % branch], cwd=modulepath) == 0
+
+parser = argparse.ArgumentParser()
+parser.add_argument("branch", help="Branch to push upstream")
+args = parser.parse_args()
+
 for m in modules:
   modulepath = os.path.abspath(m)
 
   remote_path = 'git@github.com:suitabletech/%s' %  m
+  st_branch = 'st-%s' % args.branch
+  st_staging_branch = 'st-%s-staging' % args.branch
 
-  print 'Pushing %s to %s' % (m, remote_path)
-  subprocess.check_call(['git', 'push', remote_path, 'st'], cwd=modulepath)
+  if has_branch(st_branch, modulepath):
+    print 'Pushing %s to %s' % (st_branch, remote_path)
+    subprocess.check_call(['git', 'push', remote_path, st_branch], cwd=modulepath)
+
+  if has_branch(st_staging_branch, modulepath):
+    print 'Pushing %s to %s' % (st_staging_branch, remote_path)
+    subprocess.check_call(['git', 'push', remote_path, st_staging_branch], cwd=modulepath)
   

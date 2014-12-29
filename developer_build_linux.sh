0a7860cdbe4859d3f8903005158817e1d8156b71
Daiwei Li <daiweili@suitabletech.com>
2014-12-29 14:56:00 -0800

    Update qtbase and qtdeclarative and add a developer_build_linux.sh script
    Change-Id: Idc3b30173892a90ace557a896cfdedeec315aab4

---
+++ developer_build_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+bits=$1
+./configure -developer-build -release -platform linux-g++-$bits -opensource -confirm-license -shared -nomake examples -nomake tests -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape -opengl -fontconfig -qt-xcb -gtkstyle -qt-libjpeg -no-feature-bearermanagement
+make -j8

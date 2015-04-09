631f1cfb8e898a37f541b28c6aced9847afc23e3
Daiwei Li <daiweili@suitabletech.com>
2015-04-09 12:09:11 -0700

    Update st_build_android.sh to build without dbus

--- st_build_android.sh
+++ st_build_android.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -e
-version=`$TEAMCITY_GIT_PATH rev-parse HEAD`
+git_bin=${TEAMCITY_GIT_PATH:-"git"}
+version=`$git_bin rev-parse HEAD`
 install_dir=$PWD/$version
 os=$1
 arch=${2:-armeabi-v7a}
 
-./configure -prefix $install_dir -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors -no-feature-bearermanagement
+./configure -prefix $install_dir -release -opensource -confirm-license -shared -xplatform android-g++ -nomake tests -nomake examples -android-arch $arch -android-ndk $ANDROID_NDK_ROOT -android-sdk $ANDROID_SDK_ROOT -android-ndk-host $ANDROID_NDK_HOST -android-toolchain-version 4.8 -skip qttranslations -skip qtwebkit -no-warnings-are-errors -no-feature-bearermanagement -no-dbus
 
 make -j8
 

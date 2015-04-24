e77b8957667c37a7adb6dde92c79c30d4fa14d43
Daiwei Li <daiweili@gmail.com>
2015-04-24 15:06:16 -0700

    Update st_build_linux.sh
    Don't build with dbus

--- st_build_linux.sh
+++ st_build_linux.sh
@@ -7,7 +7,7 @@ arch=$2
 version=`git rev-parse HEAD`
 install_dir=$PWD/$version
 
-./configure -prefix $install_dir -release -platform linux-g++-$bits -opensource -confirm-license -shared -nomake examples -nomake tests -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape -opengl -fontconfig -qt-xcb -gtkstyle -qt-libjpeg -no-feature-bearermanagement
+./configure -prefix $install_dir -release -platform linux-g++-$bits -opensource -confirm-license -shared -nomake examples -nomake tests -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape -opengl -fontconfig -qt-xcb -gtkstyle -qt-libjpeg -no-feature-bearermanagement -no-dbus
 
 make -j8
 

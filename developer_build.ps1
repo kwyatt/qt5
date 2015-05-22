541704d59d019178a51ed6274c1c029fce219d51
Daiwei Li <daiweili@suitabletech.com>
2015-05-22 11:19:43 -0700

    Revert "Build with VS2013 on Windows"
    We need to build some more changes with VS2010. We can put back
    the commit once we're ready to go with VS2013.
    
    This reverts commit e17d54e70d6d57eb49a3d78355027dd70c6d3e5b.

--- developer_build.ps1
+++ developer_build.ps1
@@ -1,3 +1,3 @@
-.\configure.bat -release -developer-build -opensource -confirm-license -shared -nomake examples -nomake tests -mp -angle -D QT_NO_BEARERMANAGEMENT -no-compile-examples -skip qtwebkit -platform win32-msvc2013
-jom\jom
-# nmake
+.\configure.bat -release -developer-build -opensource -confirm-license -shared -nomake examples -nomake tests -mp -angle -D QT_NO_BEARERMANAGEMENT -no-compile-examples -skip qtwebkit -skip qttools
+#jom
+nmake
--- st_build_windows_common.ps1
+++ st_build_windows_common.ps1
@@ -3,18 +3,23 @@ $osname = $args[0]
 # Grab ICU, which is required for building webkit
 echo "downloading ICU..."
 $webclient = New-Object System.Net.WebClient
-$webclient.DownloadFile("http://repo.suitabletech.com/downloads/icu_53_1_msvc_2013_${osname}_devel.zip", "$(get-location)/icu.zip")
+$webclient.DownloadFile("http://repo.suitabletech.com/downloads/icu4c-50_1_2-$osname-msvc10.zip", "$(get-location)/icu.zip")
 
 # Unzip icu
 echo "unzipping ICU..."
 & ./unzip.exe "icu.zip"
 
-$icu_libdir = "$(get-location)\icu53_1\lib"
-$icu_bindir = "$(get-location)\icu53_1\bin"
+$icu_libdir = "$(get-location)\icu\lib"
+$icu_bindir = "$(get-location)\icu\bin"
+if ($osname -eq "win64")
+{
+  $icu_libdir = "$(get-location)\icu\lib64"
+  $icu_bindir = "$(get-location)\icu\bin64"
+}
 
 # Grab OpenSSL, which is required for https support
 echo "downloading openssl..."
-$webclient.DownloadFile("http://repo.suitabletech.com/downloads/openssl-1.0.2a-$osname-msvc2013.zip", "$(get-location)/openssl.zip")
+$webclient.DownloadFile("http://repo.suitabletech.com/downloads/openssl-1.0.1h-$osname.zip", "$(get-location)/openssl.zip")
 
 echo "unzipping openssl..."
 & ./unzip.exe openssl.zip
@@ -23,8 +28,7 @@ $version = $(git rev-parse HEAD)
 echo configuring ...
 # We build with -no-icu and then enable it manually for QtWebKit. This means QtCore does not end up
 # with an ICU dependency, so we can ship installers without ICU (which is huge)
-
-.\configure.bat -debug-and-release -force-debug-info -opensource -confirm-license -shared -nomake examples -nomake tests -mp -no-icu -angle -openssl-linked OPENSSL_LIBS="-lssleay32 -llibeay32" -prefix "$(get-location)\$version" -I "$(get-location)\icu\include" -L "$icu_libdir" -I "$(get-location)\openssl\include" -L "$(get-location)\openssl\lib" -D QT_NO_BEARERMANAGEMENT -platform win32-msvc2013
+.\configure.bat -debug-and-release -force-debug-info -opensource -confirm-license -shared -nomake examples -nomake tests -mp -no-icu -angle -openssl-linked OPENSSL_LIBS="-lssleay32 -llibeay32" -prefix "$(get-location)\$version" -I "$(get-location)\icu\include" -L "$icu_libdir" -I "$(get-location)\openssl\include" -L "$(get-location)\openssl\lib" -D QT_NO_BEARERMANAGEMENT
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }
 
@@ -40,7 +44,6 @@ if ($LastExitCode -ne 0) { exit $LastExitCode }
 echo building qtwebkit...
 # Configure and build qtwebkit separately, as it needs ICU
 cd qtwebkit
-
 & ../qtbase/bin/qmake QT_CONFIG+=icu
 
 if ($LastExitCode -ne 0) { exit $LastExitCode }

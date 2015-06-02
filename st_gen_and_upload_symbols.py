cb40ad7f68ca2290f60ab7f4d5655dfa868d037f
Daiwei Li <daiweili@suitabletech.com>
2015-06-02 11:38:58 -0700

    Make pull request suggested changes - Remove unnecessary comments - Rename env variable

--- st_gen_and_upload_symbols.py
+++ st_gen_and_upload_symbols.py
@@ -10,18 +10,17 @@ import subprocess
 import shutil
 
 # This script assumes that you have a 'sw-dev' checkout at the same level as this script (unless you pass --swdev)
+# or have set the env variable QT_BUILD_SWDEV (useful if you're trying to run st_build locally and have sw-dev checked
+# out elsewhere)
 # and that the working directory is the qt5 build directory (which is the qt5 repo directory by default)
-
 if __name__ == '__main__':
-    ##### Parse arguments #####
-    swdev_default = os.environ.get('TEXBUILD_ROOT', 'sw-dev')
+    swdev_default = os.environ.get('QT_BUILD_SWDEV', 'sw-dev')
     parser = argparse.ArgumentParser()
     parser.add_argument('--swdev', dest='swdev', action='store', default=swdev_default, type=str)
     parser.add_argument('--os', dest='os', action='store', default=None, type=str,
                         choices=['win32', 'win64', 'macosx', 'linux', 'android', 'ios'], required=True)
     args = parser.parse_args()
 
-    ##### Check arguments #####
     if (args.os == 'win64'):
         logging.warning("Symbol upload for x64 not yet supported. See http://code.google.com/p/google-breakpad/issues/detail?id=427")
         sys.exit(0)
@@ -29,11 +28,9 @@ if __name__ == '__main__':
         logging.error("sw-dev folder does not exist.")
         sys.exit(1)
 
-    ##### Utility function #####
     def swdevpath(path):
         return os.path.join(args.swdev, path)
 
-    ##### Set filepath for symbol dumper #####
     win_dump_syms = swdevpath('stacks/texas_videoconf/third_party/third_party/breakpad/tools/windows/binaries/dump_syms.exe')
     linux_dump_syms = swdevpath('stacks/texas_videoconf/third_party/third_party/breakpad/tools/linux/dump_syms/dump_syms')
     mac_dump_syms = swdevpath('stacks/texas_videoconf/third_party/third_party/breakpad/tools/mac/dump_syms/build/Release/dump_syms')
@@ -46,7 +43,6 @@ if __name__ == '__main__':
             'ios': mac_dump_syms,
             }
 
-    ##### Set filepath for symbol builder #####
     linux_build_dump_syms = swdevpath('build_scripts/build_dump_syms_linux.sh')
     mac_build_dump_syms = swdevpath('build_scripts/build_dump_syms_osx.sh')
     build_dump_syms = {
@@ -58,22 +54,17 @@ if __name__ == '__main__':
             'ios': mac_build_dump_syms,
             }
 
-    ##### Delete old symbols #####
     shutil.rmtree('symbols', True)
 
-    ##### Script paths #####
     symbolstore = swdevpath('stacks/texas_videoconf/third_party/third_party/breakpad/tools/symbolstore.py')
     symbolupload = swdevpath('stacks/texas_videoconf/scripts/upload_symbols.py')
 
-    ##### Build symbols #####
     build = build_dump_syms[args.os]
     if (build):
         subprocess.check_call([build])
 
-    ##### Store symbols #####
     cmd = ['python', symbolstore, dump_syms[args.os], 'symbols', '.']
     subprocess.check_call(cmd)
 
-    ##### Upload symbols #####
     cmd = ['python', symbolupload, 'symbols', '-o', args.os, '-c', 'Final', '--software-channel', 'qt-symbols', '--software-version', '1']
     subprocess.check_call(cmd)

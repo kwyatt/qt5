ef488fce7175584c91060e11c89b939b0c4698a4
Daiwei Li <daiweili@suitabletech.com>
2015-12-14 17:12:40 -0800

    Update submodules with changes to support nacl

---
+++ st_build_nacl.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+version=`git rev-parse HEAD`
+install_dir=$PWD/$version
+# Need both for OSX (os=osx, plat=mac)
+os=$1
+plat=$2
+
+./qtbase/nacl-configure ${plat}_pnacl release 64 x86_64 -prefix $install_dir
+make -j6 module-qtbase module-qtdeclarative module-qtmultimedia module-qt3d module-qtsvg module-qtxmlpatterns module-qtquickcontrols module-qtgraphicaleffects
+make module-qtbase-install_subtargets module-qtdeclarative-install_subtargets module-qtmultimedia-install_subtargets module-qt3d-install_subtargets module-qtsvg-install_subtargets module-qtxmlpatterns-install_subtargets module-qtquickcontrols-install_subtargets module-qtgraphicaleffects-install_subtargets
+
+tar cvzf qt-$version-$os-nacl.tar.gz ./$version
+
+# Delete the version folder, since the way teamcity cleans things having a folder that's
+# also a revision is bad
+rm -rf ./$version

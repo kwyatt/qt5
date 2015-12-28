5a0a416f1090433dc9ef869b5409747031d68483
Daiwei Li <daiweili@suitabletech.com>
2015-12-28 12:12:54 -0800

    Link against openssl for Android and nacl

--- developer_build_nacl.sh
+++ developer_build_nacl.sh
@@ -15,6 +15,8 @@ if [[ "$platform" == 'unknown' ]]; then
   exit 1
 fi
 
-./qtbase/nacl-configure ${platform}_pnacl release 64 x86_64 -developer-build
+source set_swdev.sh
+
+./qtbase/nacl-configure ${platform}_pnacl release 64 x86_64 -developer-build -openssl-linked -I$SWDEV/stacks/texas_videoconf/third_party/third_party/openssl/openssl/include
 
 make -j6 module-qtbase module-qtdeclarative module-qtmultimedia module-qt3d module-qtsvg module-qtxmlpatterns module-qtquickcontrols module-qtgraphicaleffects

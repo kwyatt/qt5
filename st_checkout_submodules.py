a3c74a839603aed01fb603008b8e927b569c6d64
Jason Erb <erb@suitabletech.com>
2016-02-09 00:33:59 -0500

    Fixed st_checkout_submodules.py: wasn't checking out upstream tags.

--- st_checkout_submodules.py
+++ st_checkout_submodules.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-"""Check out submodules to the same branch as the root module."""
+"""Check out submodules to the same version as the root module."""
 
 import os
 import os.path
@@ -11,12 +11,22 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 
 def main():
     branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"], cwd=script_dir).strip();
-    print "Checking out each submodule to %s..." % branch
 
+    prefix = "st-"
+    prefix_length = len(prefix)
+    if branch[:prefix_length] != prefix:
+        print "ERROR: Branch \"%s\" invalid; must start with \"%s\"." % (branch, prefix)
+        exit(1)
+    version = branch[prefix_length:]
+    upstream_tag = "v%s" % version
+    print "Checking out each submodule to version %s..." % version
+
+    submodule_prefix = "qt"
+    submodule_prefix_length = len(submodule_prefix)
     for file_name in os.listdir(script_dir):
         submodule_dir = os.path.join(script_dir, file_name)
         if os.path.isdir(submodule_dir):
-            if file_name[0:2] == "qt":
+            if file_name[:submodule_prefix_length] == submodule_prefix:
                 submodule = file_name
                 print "=== Submodule \"%s\"..." % submodule
 
@@ -28,9 +38,11 @@ def main():
                     else:
                         print "ERROR: Could not check out branch \"%s\" from this submodule! If it doesn't exist, please create it." % branch
                 elif remote_host == "code.qt.io":
-                    print "This submodule has not been forked into our Git repository."
+                    print "This submodule has not been forked into our Git repository. Attempting to check out upstream tag %s..." % upstream_tag
+                    if 0 != subprocess.call(["git", "checkout", upstream_tag], cwd=submodule_dir):
+                        print "No upstream tag \"%s\" in this submodule." % upstream_tag
                 else:
-                    print "This submodule has an unknown remote URL: %s" % remote_url
+                    print "ERROR: This submodule has an unknown remote host. URL: %s" % remote_url
 
 if __name__ == '__main__':
     main()

41d75b4ac21526f1a8c9735684be61bf27b37bff
Jason Erb <erb@suitabletech.com>
2016-02-24 19:50:43 -0800

    Disabled PCH for linux developer build.
    Disabled PCH after getting incremental build failures due to out-of-date
    PCH files.

--- st_developer_build_linux.sh
+++ st_developer_build_linux.sh
@@ -9,7 +9,7 @@ cd "$BUILD_DIRECTORY"
 
 bits=${1:-64}
 
-"$SOURCE_DIRECTORY/configure" -release -force-debug-info -developer-build -opensource -confirm-license -shared -platform linux-g++-$bits -qt-libjpeg -qt-xcb -gtkstyle -nomake examples -nomake tests -no-compile-examples -fontconfig -no-dbus -no-feature-bearermanagement -opengl -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape
+"$SOURCE_DIRECTORY/configure" -release -force-debug-info -developer-build -opensource -confirm-license -shared -platform linux-g++-$bits -qt-libjpeg -qt-xcb -gtkstyle -nomake examples -nomake tests -no-compile-examples -fontconfig -no-pch -no-dbus -no-feature-bearermanagement -opengl -xkb -xinput -xrender -xrandr -xfixes -xcursor -xinerama -xshape
 
 export SOURCE_ROOT="$SOURCE_DIRECTORY/qtwebkit/Source/WebCore"
 

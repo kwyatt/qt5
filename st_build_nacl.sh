cf7eef4e11419d5abd5ab7933285f77365fecd5e
Jason Erb <erb@suitabletech.com>
2016-03-04 12:16:01 -0500

    Reordered NaCl configure lines for consistency; no functional change.

--- st_build_nacl.sh
+++ st_build_nacl.sh
@@ -10,7 +10,7 @@ plat=$2
 
 source st_set_swdev.sh
 
-./qtbase/nacl-configure ${plat}_pnacl release 64 x86_64 -prefix $install_dir -commercial -openssl-linked -I "$SW_DEV/stacks/texas_videoconf/third_party/third_party/openssl/openssl/include"
+./qtbase/nacl-configure ${plat}_pnacl release 64 x86_64 -prefix $install_dir -commercial -I "$SW_DEV/stacks/texas_videoconf/third_party/third_party/openssl/openssl/include" -openssl-linked
 
 make -j6 module-qtbase module-qtdeclarative module-qtmultimedia module-qt3d module-qtsvg module-qtxmlpatterns module-qtquickcontrols module-qtgraphicaleffects
 
